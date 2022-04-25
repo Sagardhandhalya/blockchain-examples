989de1ec91d0ba50c4e86a79d3e62ab34725143b
Sagardhandhalya <dhandhalyasagar0@gmail.com>
2022-04-25 22:17:23 +0530

    version update

--- eth-todo-list/truffle-config.js
+++ eth-todo-list/truffle-config.js
@@ -4,7 +4,6 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "5777", // Match any network id
-      from: "0x84aAA6D923e7dE9b0c0be3718abAa0CDd3031c5a", // default address to use for any transaction Truffle makes during migrations
     },
   },
   compilers: {
